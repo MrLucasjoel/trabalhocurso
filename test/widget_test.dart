52d7f81582a58b3f6033681df0adc444e87041cc
LUCAS JOEL PFLEGER <lucas_j_pfleger@estudante.sesisenai.org.br>
2025-10-20 17:04:11 -0300

    criação da pasta app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:trabalhocurso/main.dart';
+import 'package:trabalhocurso/src/app/myApp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
